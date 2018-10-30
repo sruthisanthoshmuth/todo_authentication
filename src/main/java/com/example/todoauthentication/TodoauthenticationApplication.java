ce215400097cfd2b7fed890f055a41d83d978674
sruthi <sruthi@techgentsia.com>
2018-10-30 11:54:07 +0530

    --project

---
+++ src/main/java/com/example/todoauthentication/TodoauthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.example.todoauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoauthenticationApplication.class, args);
+	}
+}
